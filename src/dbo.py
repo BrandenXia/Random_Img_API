538902667af4d3e46b470209d80413b7d55934d4
BrandenXia <brandenxia@163.com>
2022-10-19 13:40:52 -0700

    replace %s with ? to prevent sql injection

--- src/dbo.py
+++ src/dbo.py
@@ -1,5 +1,4 @@
 import sqlite3
-import os
 
 # connect to database
 database = sqlite3.connect("./img_info.sqlite3")
@@ -30,17 +29,17 @@ def delete(path: str):
 def search(type: str = None, img_x: int = None, img_y: int = None, needed: str = "*"):
     search_args = []
     if type is not None:
-        search_args.append(f"""TYPE = \'{type}\'""")
+        search_args.append("TYPE = \'%s\'" % type)
     if img_x is not None:
         if img_x != "?":
-            search_args.append(f"""img_x = \'{img_x}\'""")
+            search_args.append("img_x = \'%s\'" % img_x)
     if img_y is not None:
         if img_y != "?":
-            search_args.append(f"""img_y = \'{img_y}\'""")
+            search_args.append("img_y = \'%s\'" % img_y)
     if len(search_args) == 0:
-        res = cursor.execute("SELECT %s FROM img" % needed)
+        res = cursor.execute("SELECT ? FROM img", needed)
     elif len(search_args) == 1:
-        res = cursor.execute("SELECT %s FROM img WHERE %s" % needed, search_args[0])
+        res = cursor.execute("SELECT ? FROM img WHERE ?", (needed, search_args[0]))
     else:
-        res = cursor.execute("SELECT %s FROM img WHERE %s" % needed, " AND ".join(search_args))
+        res = cursor.execute("SELECT ? FROM img WHERE ?", (needed, " AND ".join(search_args)))
     return res.fetchall()
