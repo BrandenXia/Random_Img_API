d69714553c680070bfbaae2d546f52ad8c2960b7
BrandenXia <brandenxia@163.com>
2022-10-27 02:45:26 -0700

    Put working directory into a separate branch

--- working/Class.py
+++
@@ -1,8 +0,0 @@
-class img:
-    def __init__(self, id, name, img_type, img_format, img_x, img_y):
-        self.ID = id
-        self.name = name
-        self.type = img_type
-        self.format = img_format
-        self.img_x = img_x
-        self.img_y = img_y
