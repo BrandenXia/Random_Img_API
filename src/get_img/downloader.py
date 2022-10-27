77e4eaca51c1715319d329a045599c8bd42bfe1c
BrandenXia <brandenxia@163.com>
2022-10-26 21:19:06 -0700

    Change download back to one thread because it is unstable

--- src/get_img/downloader.py
+++ src/get_img/downloader.py
@@ -1,70 +1,45 @@
 import requests
 import os
-import time
-import multitasking
 
-from rich.progress import Progress
+from rich.progress import Progress, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn, TimeRemainingColumn
 from rich.traceback import install
 from retry import retry
 
-from src import config
-
 install()
+progress = Progress(TextColumn("[bold blue]{task.fields[filename]}"),
+                    BarColumn(bar_width=None),
+                    "[progress.percentage]{task.percentage:>3.1f}%",
+                    "•",
+                    DownloadColumn(),
+                    "•",
+                    TransferSpeedColumn(),
+                    "•",
+                    TimeRemainingColumn())
 
-download_config = config.Config("download.json")
-connection_num = download_config.get("connection_num")
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/91.0.4472.124 Safari/537.36"}
 
 
-def split(file_size: int, connection: int) -> list[tuple[int, int]]:
-    step = file_size // connection + 1
-    download_range = []
-    for i in range(0, connection):
-        start = i * step
-        end = start + step - 1
-        if end > file_size:
-            end = file_size
-        download_range.append((start, end))
-    return download_range
-
-
+@retry(tries=3, delay=1)
 def download(url: str, path: str, filename: str) -> None:
-    header = requests.head(url, headers=headers)
-    file_size = header.headers.get('Content-Length')
-
-    if file_size is not None:
-        file_size = int(file_size)
-    else:
-        raise Exception("File size is None")
-
     if not os.path.exists(path):
-        os.mkdir(path)
+        os.makedirs(path)
 
-    file = open(os.path.join(path, filename), "wb")
+    try:
+        header = requests.head(url, headers=headers)
+    except requests.exceptions.ConnectionError:
+        raise requests.exceptions.ConnectionError("Connection failed")
+    try:
+        total_size = int(header.headers.get("Content-Length"))
+    except TypeError:
+        raise TypeError("The url is not a valid image url")
 
-    with Progress() as progress:
-        task = progress.add_task("[cyan]Downloading %s" % filename, total=file_size)
+    response = requests.get(url, headers=headers, stream=True)
+    chunk_size = 1024
 
-        @retry(tries=3, delay=1)
-        @multitasking.task
-        def download_thread(start: int, end: int) -> None:
-            _headers = headers.copy()
-            _headers["Range"] = "bytes=%d-%d" % (start, end)
-
-            response = requests.get(url, headers=_headers, stream=True)
-            chunk_size = 1024
-            chunks = []
+    with progress:
+        task_id = progress.add_task("Download", filename=filename, total=total_size)
+        with open(os.path.join(path, filename), "wb") as f:
             for data in response.iter_content(chunk_size=chunk_size):
-                chunks.append(data)
-                progress.update(task, advance=chunk_size)
-            file.seek(start)
-            for chunk in chunks:
-                file.write(chunk)
-
-        download_range = split(file_size, connection_num)
-        for start, end in download_range:
-            download_thread(start, end)
-            time.sleep(0.01)
-        multitasking.wait_for_tasks()
-    file.close()
+                f.write(data)
+                progress.update(task_id, advance=len(data))
