9d0d9b8ba81e4cbfd1cc640d81ecb4639b51b8c9
BrandenXia <brandenxia@163.com>
2022-10-27 02:41:15 -0700

    create new working branch

--- end.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Stop the server
-kill "$(pgrep -o -f -l 'python run.py' | awk '{print $1}')"
--- working/test.py
+++
@@ -1,7 +0,0 @@
-import dbo
-import PIL
-import Class
-
-dbo.init()
-first = Class.img("1", "test", "acg", "png", 1920, 1080)
-dbo.insert_img(first)
