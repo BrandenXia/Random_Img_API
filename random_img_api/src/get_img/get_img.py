1cc9d5870689986038edce62204f6a090602a771
BrandenXia <brandenxia@163.com>
2022-10-29 20:25:11 -0700

    feat: Add avatar generator
    Support automatically generate avatar using pydenticon module

--- random_img_api/src/get_img/get_img.py
+++ random_img_api/src/get_img/get_img.py
@@ -4,18 +4,20 @@ import os
 from PIL import Image
 from rich import print
 
-from random_img_api.src import dbo, config
-from random_img_api.src.get_img import get_url, downloader
+from random_img_api.src import dbo
+from random_img_api.src.config import config
+from random_img_api.src.get_img import get_url, downloader, gen_avatar
 
 # init database
 dbo.init()
 
 # get config
 download_config = config.Config("download.json")
-download_path = download_config.get("download_path")
+img_path = download_config.get("img_path")
 
 
 def download(type: str) -> int:
+    global img_path
     # get image url and filename
     try:
         info = get_url.get_url(type)
@@ -24,10 +26,10 @@ def download(type: str) -> int:
 
     # img name and path
     img_name = info[1]
-    img_path = os.path.join(download_path, "%s.jpg" % img_name)
+    img_path = os.path.join(img_path, "%s.jpg" % img_name)
 
     # download img
-    rt = downloader.download(info[0], download_path, "%s.jpg" % img_name)
+    rt = downloader.download(info[0], img_path, "%s.jpg" % img_name)
     # if failed, return exit code
     if rt != 0:
         return rt
@@ -38,6 +40,14 @@ def download(type: str) -> int:
     dbo.insert(info[1], type, "jpg", img_path, img_x, img_y)
 
     return 0
+
+
+def generator(type: str) -> None:
+    if type == "avatar":
+        filename = gen_avatar.gen_avatar()
+    else:
+        return
+    dbo.insert(filename, type, "png", os.path.join(img_path, filename), 200, 200)
 
 
 @click.command()
@@ -70,4 +80,9 @@ def get(type, num):
                     print("[bold green]Download canceled")
                     break
     elif action == "generate":
-        pass
+        if num == 0:
+            while True:
+                generator(type)
+        else:
+            for i in range(num):
+                generator(type)
